4b3e2879e74542461823119b67137cb2345f5c2f
Aksh-007 <akshaybarapatre54@gmail.com>
2023-01-21 11:48:25 +0530

    added basic app and dependencies

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ import express from "express";
 import dotenv from "dotenv"
 dotenv.config();
 const app = express();
+import quotes from './quotes.json' assert { type: "json" };
+
 
 
 app.get('/', (req, res) => (
     res.send("<h1>Hello world</h1> ")
 ));
+
+app.get("/quote", (req, res) => {
+    res.json(quotes);
+});
+
+app.get("/random", (req, res) => {
+    let index = Math.floor(Math.random()*quotes.length );
+    let quote = quotes[index];
+    res.status(200).json({
+        sucess:true,
+        quote
+    });
+});
 
 
 
